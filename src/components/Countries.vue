fd639413bd12032798defb4f2180a41810dcc37e
evergreen2001 <dicksonevergreenido@gmail.com>
2020-09-20 17:53:40 -0700

    fixing bugs

---
+++ src/components/Countries.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Countries',
+  created() {
+
+  },
+  data() {
+    return {
+    }
+  },
+  props: {
+   
+  },
+  methods: {
+   
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
